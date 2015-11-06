5ad5035b5a6ac525743d31b102f6a940961262e5
unknown <liyi3@LIYI3.qiku.com>
2015-11-06 16:59:32 +0800

    动态壁纸文件放到service目录

---
+++ livewp.php
@@ -0,0 +1,14 @@
+<?php
+    header('Content-Type: application/json; charset=utf-8');
+    $url_arr = array("date" => "20151010",
+                     "version_name" => "5.7.8",
+                     "version" => "455",
+                     "silent" => 0,
+                     "wholenet" => 1,
+                     "url" => "http://d.res.zhuti.qiku.com/coolshow/1508_vlife-sdk-coolpad_455_5.122.1_10091120.apk",
+                     "desc" => "Éý¼¶ÃèÊö",
+                     "md5" => ""
+     );
+ // {"date":"20151010","version_name":"5.7.8","version":"455","silent":0,"wholenet":1,"url":"http://d.res.zhuti.qiku.com/coolshow/1508_vlife-sdk-coolpad_455_5.122.1_10091120.apk","desc":"Éý¼¶ÃèÊö","md5":""} 
+    echo json_encode($url_arr);
+?>
