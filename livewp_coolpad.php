7af028597850dbf61f7c11a9abe9a3ba0ec7e454
wangweilin <wangweilin@qiku.com>
2015-12-04 18:41:07 +0800

    add livewp coolpad

---
+++ livewp_coolpad.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+$url_arr = array(
+    "date" => "20151203",
+    "version_name" => "5.7.8",
+    "version" => "455",
+    "silent" => 0,
+    "wholenet" => 1,
+    "url" => "http://d.res.zhuti.qiku.com/coolshow/vlife_20151203_coolpad.apk",
+    "desc" => "升级描述",
+    "md5" => ""
+);
+echo json_encode($url_arr);
