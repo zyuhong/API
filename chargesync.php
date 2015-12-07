16b8035e886f2d61b82fa2f4648a4c06b960f795
wangweilin <wangweilin@qiku.com>
2015-12-07 20:04:29 +0800

    支付同步接口添加更新逻辑

--- chargesync.php
+++ chargesync.php
@@ -6,14 +6,13 @@ require_once 'lib/WriteLog.lib.php';
 require_once 'tasks/charge/ChargeDb.class.php';
 
 $jsonCharge = isset($_POST['transdata'])?$_POST['transdata']:'';
-if (empty($jsonCharge)){
-	$jsonCharge = file_get_contents("php://input");//isset($_POST['charge'])?$_POST['charge']:'';
-}
-// $jsonCharge = file_get_contents("php://input");//isset($_POST['charge'])?$_POST['charge']:'';
 // $jsonCharge = '{"amt":"1.0","merid":"1001","mername":"测试","appid":"1001","appname":"应用场景","chargepoint":"","chargepointname":"","operators":"","orderdate":"2012-11-01 23:32:50","orderid":"121101233244538","ordersatus":"202","paytype":"100000000016","paytypename":"短信","phone":"","province":"","reqOrderId":"123456789","sign":"…………….."}';
+if (empty($jsonCharge)) {
+	$jsonCharge = file_get_contents("php://input");
+}
 
 Log::write('chargesync:: jsonCharge:'.$jsonCharge, 'debug');
-if(empty($jsonCharge)){
+if (empty($jsonCharge)) {
 	Log::write('chargesync:: charge is empty', 'error');
 	
 	$result = array('resultCode'=>300);
@@ -28,12 +27,23 @@ $arrChargeRecord = json_decode($jsonCharge, true);
 
 $chargeDb->setNChargeRecord($arrChargeRecord);
 $bResult = $chargeDb->recordNCharge();
-if(!$bResult){
+if (! $bResult) {
 	Log::write('chargesync:: recordNCharge failed', 'error');
 	
 	$result = array('resultCode'=>300);	
 	echo json_encode($result);
 	exit();
+}
+
+require_once 'tasks/Exorder/ExorderRecordDb.class.php';
+$exorder = isset($arrChargeRecord['reqOrderId'])?$arrChargeRecord['reqOrderId']:'';
+$resultCode = (int)(isset($arrChargeRecord['ordersatus'])?$arrChargeRecord['ordersatus']:0);
+if (! empty($exorder) && $resultCode == 200) {
+    $erDb = new ExorderRecordDb();
+    $bResult = $erDb->updateMobileExorder($exorder, 0);
+    if (! $bResult) {
+        Log::write('chargesync_qiku updateMobileExorder() failed,exorder is '.$exorder, 'debug');
+    }
 }
 
 $result = array('resultCode'=>200);
--- chargesync_qiku.php
+++ chargesync_qiku.php
@@ -9,17 +9,16 @@ require 'public/charge_sign.php';
 $jsonCharge = isset($_POST['transdata'])?$_POST['transdata']:'';
 $jsonCharge = str_replace('\\', '', $jsonCharge);
 $sign = isset($_POST['sign'])?$_POST['sign']:'';
-
 Log::write("charge=".$jsonCharge.", sign=".$sign, "debug");
-if(empty($jsonCharge)){
+if (empty($jsonCharge)) {
     Log::write('chargesync:: charge is empty', 'debug');
     echo 'FAILURE';
     exit();
 }
 
-if(!empty($sign)){
+if (! empty($sign)) {
     $result = validsign($jsonCharge, $sign);
-    if($result != 0){
+    if ($result != 0) {
         Log::write("chargesync:: charge sign fail", "debug");
         echo 'FAILURE';
         exit();
@@ -36,9 +35,20 @@ $arrChargeRecord = json_decode($jsonCharge, true);
 
 $chargeDb->setQikuChargeRecord($arrChargeRecord);
 $bResult = $chargeDb->recordNCharge();
-if(!$bResult){
+if (! $bResult) {
 	Log::write('chargesync:: recordNCharge failed', 'error');
 	$result = 'FAILURE';	
+}
+
+require_once 'tasks/Exorder/ExorderRecordDb.class.php';
+$exorder = isset($arrChargeRecord['exorderno'])?$arrChargeRecord['exorderno']:'';
+$resultCode = (int)(isset($arrChargeRecord['result'])?$arrChargeRecord['result']:1);
+if (! empty($exorder) && $resultCode == 0) {
+    $erDb = new ExorderRecordDb();
+    $bResult = $erDb->updateMobileExorder($exorder, 0);
+    if (! $bResult) {
+        Log::write('chargesync_qiku updateMobileExorder() failed,exorder is '.$exorder, 'debug');
+    }
 }
 
 echo $result;
