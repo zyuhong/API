dc3470e8c3023ac388f34b336321e65e5b2142ef
wangweilin <wangweilin@qiku.com>
2015-12-10 16:57:48 +0800

    modify mark table name

--- tasks/CoolShow/MarkSql.class.php
+++ tasks/CoolShow/MarkSql.class.php
@@ -8,6 +8,6 @@
 
 defined("SQL_SELECT_MARK_LIST")
     or define("SQL_SELECT_MARK_LIST", " SELECT res.`res_id`, res.`cooltype`, res.`mark_id`, res.`position`,mark.`url` "
-                                        ." FROM `tb_qiku_resource_mark` res "
+                                        ." FROM `tb_qiku_mark_resource` res "
                                         ." LEFT JOIN `tb_qiku_mark_list` mark ON res.`mark_id` = mark.`identity`"
                                         ." WHERE res.valid=1 and mark.valid=1 ");
