da666d6530dae120d3d757c5e6378fe5366b5d27
fulianjie <fulianjie@qiku.com>
2015-12-14 14:19:54 +0800

    add demo data

--- src/Application/Controller/Watermark.php
+++ src/Application/Controller/Watermark.php
@@ -9,51 +9,116 @@ class Watermark extends Base
 {
     public function listAction()
     {
-        $demo = [
-            'result' => true,
-            'total_number' => 100,
-            'cats' => [
-                [
-                    'id' => 1,
-                    'name' => '热点',
-                    'cat' => 'hot',
-                    'watermarks' => [
-                        [
-                            'id' => 1,
-                            'name' => '魂之挽歌',
-                            'cover' => 'http://watermark.test.os.qkcorp.net/cover1.jpg',
-                            'sort' => 1000, // desc
-                        ],
-                        [
-                            'id' => 2,
-                            'name' => '青春',
-                            'cover' => 'http://watermark.test.os.qkcorp.net/cover2.jpg',
-                            'sort' => 990, // desc
+        $cat = $this->get('cat');
+        if (!empty($cat)) {
+            $demo = [
+                'result' => true,
+                'total_number' => 100,
+                'cats' => [
+                    [
+                        'id' => 1,
+                        'name' => 'test',
+                        'cat' => $cat,
+                        'watermarks' => [
+                            [
+                                'id' => 1,
+                                'name' => '魂之挽歌',
+                                'cover' => 'http://watermark.test.os.qkcorp.net/cover1.jpg',
+                                'sort' => 1000, // desc
+                            ],
                         ]
                     ]
-                ],
-                [
-                    'id' => 2,
-                    'name' => '心情',
-                    'cat' => 'xinqing',
-                ],
-                [
-                    'id' => 3,
-                    'name' => '人像',
-                    'cat' => 'renxiang',
-                ],
-                [
-                    'id' => 4,
-                    'name' => '地点',
-                    'cat' => 'didian',
-                ],
-                [
-                    'id' => 5,
-                    'name' => '时间',
-                    'cat' => 'shijian',
-                ],
-            ]
-        ];
+                ]
+            ];
+        } else {
+            $demo = [
+                'result' => true,
+                'total_number' => 100,
+                'cats' => [
+                    [
+                        'id' => 1,
+                        'name' => '热点',
+                        'cat' => 'hot',
+                        'watermarks' => [
+                            [
+                                'id' => 1,
+                                'name' => '魂之挽歌',
+                                'cover' => 'http://watermark.test.os.qkcorp.net/cover1.jpg',
+                                'sort' => 1000, // desc
+                            ],
+                            [
+                                'id' => 2,
+                                'name' => '青春',
+                                'cover' => 'http://watermark.test.os.qkcorp.net/cover2.jpg',
+                                'sort' => 990, // desc
+                            ],
+                            [
+                                'id' => 3,
+                                'name' => 'test3',
+                                'cover' => 'http://watermark.test.os.qkcorp.net/cover2.jpg',
+                                'sort' => 980, // desc
+                            ],
+                            [
+                                'id' => 3,
+                                'name' => 'test3',
+                                'cover' => 'http://watermark.test.os.qkcorp.net/cover2.jpg',
+                                'sort' => 970, // desc
+                            ],
+                            [
+                                'id' => 4,
+                                'name' => 'test4',
+                                'cover' => 'http://watermark.test.os.qkcorp.net/cover2.jpg',
+                                'sort' => 960, // desc
+                            ],
+                            [
+                                'id' => 5,
+                                'name' => 'test5',
+                                'cover' => 'http://watermark.test.os.qkcorp.net/cover2.jpg',
+                                'sort' => 950, // desc
+                            ],
+                            [
+                                'id' => 6,
+                                'name' => 'test3',
+                                'cover' => 'http://watermark.test.os.qkcorp.net/cover2.jpg',
+                                'sort' => 940, // desc
+                            ],
+                            [
+                                'id' => 7,
+                                'name' => 'test3',
+                                'cover' => 'http://watermark.test.os.qkcorp.net/cover2.jpg',
+                                'sort' => 930, // desc
+                            ],
+                            [
+                                'id' => 8,
+                                'name' => 'test3',
+                                'cover' => 'http://watermark.test.os.qkcorp.net/cover2.jpg',
+                                'sort' => 920, // desc
+                            ],
+                        ]
+                    ],
+                    [
+                        'id' => 2,
+                        'name' => '心情',
+                        'cat' => 'xinqing',
+                    ],
+                    [
+                        'id' => 3,
+                        'name' => '人像',
+                        'cat' => 'renxiang',
+                    ],
+                    [
+                        'id' => 4,
+                        'name' => '地点',
+                        'cat' => 'didian',
+                    ],
+                    [
+                        'id' => 5,
+                        'name' => '时间',
+                        'cat' => 'shijian',
+                    ],
+                ]
+            ];
+        }
 
         $this->jsonOutput($demo);
     }
