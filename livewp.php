518be3a2edbb228db8c50e4fae2d17cc80555449
wangweilin <wangweilin@qiku.com>
2015-12-16 18:53:17 +0800

    modify vlive apk info

--- livewp.php
+++ livewp.php
@@ -1,12 +1,12 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 $url_arr = array(
-    "date" => "20151010",
+    "date" => "20151203",
     "version_name" => "5.7.8",
     "version" => "455",
     "silent" => 0,
     "wholenet" => 1,
-    "url" => "http://d.res.zhuti.qiku.com/coolshow/1508_vlife-sdk-coolpad_455_5.122.1_10091120.apk",
+    "url" => "http://d.res.zhuti.qiku.com/coolshow/vlife_20151203_qiku.apk",
     "desc" => "升级描述",
     "md5" => ""
 );
