3ba3fc8e2eea1d91d84e221ce30228f29a2de668
fulianjie <fulianjie@qiku.com>
2015-12-25 10:29:54 +0800

    fix release config

--- config/release.config.php
+++ config/release.config.php
@@ -3,7 +3,8 @@
 return [
     '/app/cdn' => 'http://d.res.zhuti.qiku.com',
     '/db/mdb/zhuti_api_db' => [
-        "host" => "10.120.253.109:3562",
+        "host" => "10.120.253.109",
+        "port" => "3562",
         "username" => "zhuti",
         "password" => "Kw_s71Ls_2t",
         "coding" => "utf8",
