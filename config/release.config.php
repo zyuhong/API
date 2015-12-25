5c4d1a33c697724d121bafb10344e964242135e9
fulianjie <fulianjie@qiku.com>
2015-12-25 10:13:00 +0800

    modify db info

--- config/release.config.php
+++ config/release.config.php
@@ -4,8 +4,8 @@ return [
     '/app/cdn' => 'http://d.res.zhuti.qiku.com',
     '/db/mdb/zhuti_api_db' => [
         "host" => "10.120.253.109:3562",
-        "user" => "zhuti",
-        "pwd" => "Kw_s71Ls_2t",
+        "username" => "zhuti",
+        "password" => "Kw_s71Ls_2t",
         "coding" => "utf8",
     ],
     '/db/name/watermark_db' => 'db_yl_themes'
