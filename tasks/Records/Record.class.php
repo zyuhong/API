015cc837e7de1287838d1bc3b508aa42cda223e1
wangweilin <wangweilin@qiku.com>
2016-01-04 21:42:44 +0800

    add new statis domain

--- tasks/Records/Record.class.php
+++ tasks/Records/Record.class.php
@@ -18,59 +18,73 @@ abstract class Record
 	public $ip;				//IP地址
 	public $net;			//网络类型
 	public $prover;			//协议版本
+    public $oversea;        //海外
+    public $language;       //语言
+    public $coolshowchannel;  //渠道
+    public $outerVersion;     //外部版本
+    public $innerVersion;       //内部版本
+    public $uiVersion;          //UI版本
 	public $insert_time;	//时间
-	
-	
-	public function __construct()
-	{	
-		$this->product 	= '';
-// 		$this->height 	= 0;
-// 		$this->width 	= 0;
-		$this->cyid		= '';
-		$this->imsi 	= '';
-		$this->meid 	= '';
-		$this->vercode 	= 0;
-		$this->kernel 	= 0;
-		$this->type 	= 0;
-		$this->cooltype = 0;
-		$this->subtype 	= 0;
-		$this->channel 	= 0;
-		$this->ip 		= '';
-		$this->net 		= '';
-		$this->prover	= 0;
-		$this->insert_time = date('Y-m-d H:i:s');
-	}
-	
-	public function setCoolType($nCoolType)
-	{
-		$this->cooltype = (int)$nCoolType;
-	}
-	
-	protected function setParam()
-	{
-		$this->vercode	= (int)(isset($_GET['versionCode'])?$_GET['versionCode']:0);
 
-		if(isset($_POST['statis'])){
-			$json_param = isset($_POST['statis'])?$_POST['statis']:'';
-		
-			$json_param = stripslashes($json_param);
-			$arr_param = json_decode($json_param, true);
-		
-			$this->product	= isset($arr_param['product'])?$arr_param['product']:'CoolUI';
-			$this->cyid 	= isset($arr_param['cyid'])?$arr_param['cyid']:'';
-			$this->imsi 	= isset($arr_param['imsi'])?$arr_param['imsi']:'';
-			$this->meid 	= isset($arr_param['meid'])?$arr_param['meid']:'';
-			$this->vercode	= isset($arr_param['versionCode'])?$arr_param['versionCode']:$this->vercode;
-// 			$this->width 	= isset($arr_param['width'])?$arr_param['width']:0;
-// 			$this->height 	= isset($arr_param['height'])?$arr_param['height']:0;
-			$this->net 		= isset($arr_param['network'])?$arr_param['network']:'net';
-			$this->prover = (int)(isset($arr_param['protocolCode'])?$arr_param['protocolCode']:0);
-		}
+    public function __construct()
+    {
+        $this->product 	= '';
+        //$this->height 	= 0;
+        //$this->width 	= 0;
+        $this->cyid		= '';
+        $this->imsi 	= '';
+        $this->meid 	= '';
+        $this->vercode 	= 0;
+        $this->kernel 	= 0;
+        $this->type 	= 0;
+        $this->cooltype = 0;
+        $this->subtype 	= 0;
+        $this->channel 	= 0;
+        $this->ip 		= '';
+        $this->net 		= '';
+        $this->prover	= 0;
+        $this->oversea = '';
+        $this->language = '';
+        $this->coolshowchannel = '';
+        $this->outerVersion = '';
+        $this->innerVersion = '';
+        $this->uiVersion = '';
+        $this->insert_time = date('Y-m-d H:i:s');
+    }
 
-		$this->kernel	= (int)(isset($_GET['kernelCode'])?$_GET['kernelCode']:0);
-		$this->ip = isset($_SERVER['HTTP_X_REAL_IP'])?$_SERVER['HTTP_X_REAL_IP']:$_SERVER['REMOTE_ADDR'];
-// 		$session = '';//session_id();
-	}
+    public function setCoolType($nCoolType)
+    {
+        $this->cooltype = (int)$nCoolType;
+    }
+
+    protected function setParam()
+    {
+        $this->vercode	= (int)(isset($_GET['versionCode']) ? $_GET['versionCode'] : 0);
+        if (isset($_POST['statis'])) {
+            $json_param = isset($_POST['statis']) ? $_POST['statis'] : '';
+            $json_param = stripslashes($json_param);
+            $arr_param = json_decode($json_param, true);
+
+            $this->product	= isset($arr_param['product']) ? $arr_param['product'] : 'CoolUI';
+            $this->cyid 	= isset($arr_param['cyid']) ? $arr_param['cyid'] : '';
+            $this->imsi 	= isset($arr_param['imsi']) ? $arr_param['imsi'] : '';
+            $this->meid 	= isset($arr_param['meid']) ? $arr_param['meid'] : '';
+            $this->vercode	= isset($arr_param['versionCode']) ? $arr_param['versionCode'] : $this->vercode;
+            //$this->width 	= isset($arr_param['width'])?$arr_param['width']:0;
+            //$this->height 	= isset($arr_param['height'])?$arr_param['height']:0;
+            $this->net 		= isset($arr_param['network']) ? $arr_param['network'] : 'net';
+            $this->prover = (int)(isset($arr_param['protocolCode']) ? $arr_param['protocolCode'] : 0);
+            $this->oversea = isset($arr_param['oversea']) ? $arr_param['oversea'] : '';
+            $this->language = isset($arr_param['language']) ? $arr_param['language'] : '';
+            $this->coolshowchannel = isset($arr_param['coolshowchannel']) ? $arr_param['coolshowchannel'] : '';
+            $this->outerVersion = isset($arr_param['outerVersion']) ? $arr_param['outerVersion'] : '';
+            $this->innerVersion = isset($arr_param['innerVersion']) ? $arr_param['innerVersion'] : '';
+            $this->uiVersion = isset($arr_param['uiVersion']) ? $arr_param['uiVersion'] : '';
+        }
+
+        $this->kernel	= (int)(isset($_GET['kernelCode']) ? $_GET['kernelCode'] : 0);
+        $this->ip = isset($_SERVER['HTTP_X_REAL_IP']) ? $_SERVER['HTTP_X_REAL_IP'] : $_SERVER['REMOTE_ADDR'];
+    }
 	
 	public function checkParam()
 	{	
