e8c44a42d5acaeed1a31c5651d1e7ffd874aab63
wangweilin <wangweilin@qiku.com>
2016-02-16 17:37:37 +0800

    update vlife engine config

--- livewp.php
+++ livewp.php
@@ -10,12 +10,12 @@ $engineCodeType = Verify::check($_GET, "engineCodeType", Type::INT);
 $channel = Verify::check($_GET, "channel", Type::INT);
 
 if ($engineCodeType == 0 && $engineCode == 0 && $channel == 0) {
-    $date = '20151203';
-    $versionName = '5.133.2';
-    $version = '461';
+    $date = '20160125';
+    $versionName = '5.133.3';
+    $version = '462';
     $silent = 1;
     $wholenet = 1;
-    $url = 'http://d.res.zhuti.qiku.com/coolshow/vlife_20151203_qiku.apk';
+    $url = 'http://d.res.zhuti.qiku.com/coolshow/vlife-release-462-20160125-142006.apk';
 } elseif ($engineCodeType == TAG_ENGINE_TYPE_TT) {
     $date = '20151225';
     $versionName = '1.1';
@@ -24,20 +24,20 @@ if ($engineCodeType == 0 && $engineCode == 0 && $channel == 0) {
     $wholenet = 1;
     $url = 'http://d.res.zhuti.qiku.com/coolshow/tiantianlocker_V1.1_2_20151225_1429.apk';
 } elseif ($engineCodeType == TAG_ENGINE_TYPE_VL && $channel == TAG_ENGINE_CHANNEL_QIKU) {
-    $date = '20151203';
-    $versionName = '5.133.2';
-    $version = '461';
+    $date = '20160125';
+    $versionName = '5.133.3';
+    $version = '462';
     $silent = 1;
     $wholenet = 1;
-    $url = 'http://d.res.zhuti.qiku.com/coolshow/vlife_20151203_qiku.apk';
+    $url = 'http://d.res.zhuti.qiku.com/coolshow/vlife-release-462-20160125-142006.apk';
 } elseif (($engineCodeType == TAG_ENGINE_TYPE_VL && $channel == TAG_ENGINE_CHANNEL_COOLPAD) or
             ($engineCodeType == TAG_ENGINE_TYPE_VL && $channel == TAG_ENGINE_CHANNEL_IVVI) ) {
-    $date = '20151222';
-    $versionName = '5.133.1';
-    $version = '461';
+    $date = '20160201';
+    $versionName = '5.133.3';
+    $version = '463';
     $silent = 1;
     $wholenet = 1;
-    $url = 'http://d.res.zhuti.qiku.com/coolshow/vlife-release-461-20151222-134929.apk';
+    $url = 'http://d.res.zhuti.qiku.com/coolshow/vlife-release-463-20160201-094114.apk';
 } else {
     exit();
 }
